b837d773e66b5644ae0fac1f9753f73f3546076c
halait <40286718+halait@users.noreply.github.com>
2023-03-24 02:54:17 -0400

    name change

--- docs/yartl_engine.js
+++ docs/yartl_engine.js
@@ -170,7 +170,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('rust_template_engine_bg.wasm', import.meta.url);
+        input = new URL('yartl_engine_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
